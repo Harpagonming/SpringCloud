4ca42efb9db18af62a222ccffaf7e0ece577c264
Cao, Zhenming <cao.zhm@neusoft.com>
2020-05-13 11:05:53 +0800

    config-server修改

--- config-server/src/main/java/com/zhenming/demo/config/server/ConfigServer.java
+++ config-server/src/main/java/com/zhenming/demo/config/server/ConfigServer.java
@@ -1,18 +1,20 @@
 package com.zhenming.demo.config.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServer {
+    private static final Logger log = LoggerFactory.getLogger(ConfigServer.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
+        log.info("Program has been launched successfully...");
     }
 }
