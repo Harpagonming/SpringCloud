4906aab279ca9e4b5ef3760257f9ca970051abfe
Cao, Zhenming <cao.zhm@neusoft.com>
2020-05-26 09:22:55 +0800

    修改gitignore文件，忽略日志

---
+++ src/test/java/com/zhenming/demo/zookeeperconsumer/ZookeeperConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhenming.demo.zookeeperconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zookeeper-producer/src/main/java/com/zhenming/demo/zookeeper/ZookeeperProducerApplication.java
@@ -0,0 +1,26 @@
+package com.zhenming.demo.zookeeper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ZookeeperProducerApplication {
+    private static final Logger log = LoggerFactory.getLogger(ZookeeperProducerApplication.class);
+
+    @RequestMapping("/")
+    public Object home() {
+        return "Success";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperProducerApplication.class, args);
+        log.info("Program has been launched successfully...");
+    }
+}
