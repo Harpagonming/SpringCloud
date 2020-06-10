ed6edf5e2b5f742c94737a704592f3966906130a
Cao, Zhenming <cao.zhm@neusoft.com>
2020-06-10 15:33:43 +0800

    zookeeper demo完成

--- zookeeper-consumer/src/main/java/com/zhenming/demo/cloud/zookeeper/ZookeeperConsumerApplication.java
+++ zookeeper-consumer/src/main/java/com/zhenming/demo/cloud/zookeeper/ZookeeperConsumerApplication.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class ZookeeperConsumerApplication {
   private static final Logger log = LoggerFactory.getLogger(ZookeeperConsumerApplication.class);
 
--- zookeeper-consumer/src/main/java/com/zhenming/demo/cloud/zookeeper/web/BaseController.java
+++ zookeeper-consumer/src/main/java/com/zhenming/demo/cloud/zookeeper/web/BaseController.java
@@ -1,8 +1,6 @@
 package com.zhenming.demo.cloud.zookeeper.web;
 
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import com.zhenming.demo.cloud.zookeeper.client.ConsumerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,19 +9,10 @@ import javax.annotation.Resource;
 @RestController
 public class BaseController {
   @Resource
-  private LoadBalancerClient loadBalancer;
+  private ConsumerClient client;
 
-  @Resource
-  private DiscoveryClient discoveryClient;
-
-  @RequestMapping("/discovery")
-  public Object discovery() {
-    ServiceInstance instance = loadBalancer.choose("zookeeper-producer");
-    return loadBalancer.choose("zookeeper-producer");
-  }
-
-  @RequestMapping("/all")
-  public Object all() {
-    return discoveryClient.getServices();
+  @RequestMapping("/invoke")
+  public Object invoke() {
+    return client.home();
   }
 }
