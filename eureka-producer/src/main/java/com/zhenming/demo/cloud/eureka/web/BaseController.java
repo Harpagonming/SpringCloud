34a0904bae0264031685a817fbcb23c579480ba5
Cao, Zhenming <cao.zhm@neusoft.com>
2020-08-11 17:28:51 +0800

    集成sleuth

--- eureka-producer/src/main/java/com/zhenming/demo/cloud/eureka/web/BaseController.java
+++ eureka-producer/src/main/java/com/zhenming/demo/cloud/eureka/web/BaseController.java
@@ -1,20 +1,18 @@
 package com.zhenming.demo.cloud.eureka.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 public class BaseController {
+  public static final Logger log = LoggerFactory.getLogger(BaseController.class);
+
   @GetMapping("/base/test")
-  public Object home(HttpServletRequest request, @RequestParam("id") String id) {
-//    Enumeration<String> enumeration = request.getHeaderNames();
-//    while (enumeration.hasMoreElements()){
-//      System.out.println(enumeration.nextElement());
-//    }
-    System.out.println(id);
+  public Object home(@RequestParam("id") String id) {
+    log.info("/base/test\tparameter: id = {}", id);
     return id;
   }
 }
