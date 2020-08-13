baa96eb1995a789f5a3625cb8d9d6deb363ea680
uf042417 <zhayw@neusoft.com>
2020-08-13 09:46:09 +0800

    修改zookeeper instance地址

--- zookeeper-producer/src/main/java/com/zhenming/demo/cloud/zookeeper/web/BaseController.java
+++ zookeeper-producer/src/main/java/com/zhenming/demo/cloud/zookeeper/web/BaseController.java
@@ -1,12 +1,25 @@
 package com.zhenming.demo.cloud.zookeeper.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 public class BaseController {
-  @RequestMapping("/")
-  public Object home() {
-    return "Hello World!";
-  }
+    public static final Logger log = LoggerFactory.getLogger(BaseController.class);
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    public Object home() {
+        log.info(">>>>>>>>>>>>>>>>>>>> 调用 <<<<<<<<<<<<<<<<<<<<");
+        String result = restTemplate.getForEntity("http://127.0.0.1:8680/test", String.class).getBody();
+        log.info(result);
+        return "Hello World!";
+    }
 }
